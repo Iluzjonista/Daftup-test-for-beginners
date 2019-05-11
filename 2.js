c3b2006ec803df45b194c014b7265b356d35b91a
a <a@a.pl>
2019-05-11 23:02:54 +0200

    upload

---
+++ 2.js
@@ -0,0 +1,17 @@
+var modal = document.getElementsByClassName('modal')[0];
+var btn_open = document.getElementsByClassName('button')[0];
+var btn_close = document.getElementsByClassName('button')[1];
+
+// When the user clicks the button, open the modal 
+btn_open.onclick = function() {
+    modal.style.display = "block";
+}
+btn_close.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "block";
+  }
+}
