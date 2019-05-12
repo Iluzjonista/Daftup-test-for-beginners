7a118943bcb6f1e9371926c9f1974b33ed7dd1b4
a <a@a.pl>
2019-05-12 17:09:07 +0200

    fix

--- 2.js
+++ 2.js
@@ -1,19 +1,19 @@
-var modal = document.getElementsByClassName('modal')[0];
-var btn_open = document.getElementsByClassName('button')[0];
-var btn_close = document.getElementsByClassName('button')[1];
+var modal = document.getElementsByClassName("modal")[0];
+var btn_open = document.getElementsByClassName("button")[0];
+var btn_close = document.getElementsByClassName("button")[1];
 
-// When the user clicks the button, open the modal 
+// When the user clicks the button, open the modal
 btn_open.onclick = function() {
-    modal.style.display = "block";
-}
+  modal.style.display = "block";
+};
 
-// When the user clicks the button, close the modal 
+// When the user clicks the button, close the modal
 btn_close.onclick = function() {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 // When the user clicks anywhere outside of the modal, don't close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "block";
   }
-}
+};
